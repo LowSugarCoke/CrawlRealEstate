4f9fffb8f073e5bb9869a1dd23c2fc7408cdc33b
Jack Lee <jack20499@gmail.com>
2022-06-04 14:29:45 +0800

    add crawl website

---
+++ realestate_website.py
@@ -0,0 +1,26 @@
+
+import requests
+import re
+
+# 591
+# https://market.591.com.tw/
+
+# 樂居
+# https://www.leju.com.tw/
+
+# 樂屋
+# https://www.rakuya.com.tw/realprice/result?city=0&zipcode=106&search=community&keyword=%E6%88%90%E5%BE%B7%E8%8F%81%E9%81%B8%E9%9B%86&sort=11
+
+
+r = requests.get(
+    'https://www.leju.com.tw/page_search_result?oid=L4021038888b2d9')
+r.encoding = 'utf-8'
+if r.status_code == 200:
+    # print(r.content.decode('utf-8'))
+
+    subject = re.findall(r'本社區共有',
+                         r.content.decode('utf-8'))
+    print(subject)
+
+
+print("status code = ", r.status_code)
