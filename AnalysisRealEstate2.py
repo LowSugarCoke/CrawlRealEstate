d200ae453dafc1c7ec63701a23cddbf11496593a
Jack Lee <jack20499@gmail.com>
2022-06-23 11:01:46 +0800

    Modify bug some delete redudant code

--- AnalysisRealEstate2.py
+++ AnalysisRealEstate2.py
@@ -1,13 +1,12 @@
-from distutils.command.build import build
-from itertools import groupby
 import os
 import pandas as pd
-import plotly.express as px
 from matplotlib.font_manager import FontProperties
-import matplotlib.pyplot as plt
-import difflib
-import csv
 
+# 自定義需要統計的縣市
+location_all = ["台北市", "桃園市", "台中市", "新北市", "苗栗縣", "花蓮縣", "台東縣",
+                "基隆市", "南投縣", "澎湖縣", "台南市", "彰化縣", "陽明山", "高雄市",
+                "雲林縣", "金門縣", "嘉義縣", "連江縣", "宜蘭縣", "台南縣", "嘉義市",
+                "高雄縣", "新竹市", "新竹縣", "屏東縣"]
 
 # Part 1 資料匯入及處裡
 
@@ -15,139 +14,74 @@ import csv
 myfont = FontProperties(fname=r'./TaipeiSansTCBeta-Regular.ttf')
 myfont.set_size(15)
 
-location_all = ["台北市"]
-
+# 所有縣市對應的英文
 location_str = """台北市 A 苗栗縣 K 花蓮縣 U
                   台中市 B 台中縣 L 台東縣 V
                   基隆市 C 南投縣 M 澎湖縣 X
                   台南市 D 彰化縣 N 陽明山 Y
                   高雄市 E 雲林縣 P 金門縣 W
-                  台北縣 F 嘉義縣 Q 連江縣 Z
+                  新北市 F 嘉義縣 Q 連江縣 Z
                   宜蘭縣 G 台南縣 R 嘉義市 I
                   桃園市 H 高雄縣 S 新竹市 O
                   新竹縣 J 屏東縣 T"""
 
-# List切片用法
-# print(location_str.split())
-# print(location_str.split()[::2])  # 步長為2
-# print(location_str.split()[1::2])  # 從1開始步長為2
-
-
-print(location_str.split()[::2])
-print(location_str.lower().split()[1::2])
-
-# '台北市', 'A', '苗栗縣', 'K', '花蓮縣', 'U', '台中市', 'B', '台中縣', 'L', '台東縣', 'V', '基隆市', 'C', '南投縣', 'M', '澎湖縣', 'X', '台南市', 'D', '彰化縣', 'N', '陽明山', 'Y', '高雄市', 'E', '
-# 雲林縣', 'P', '金門縣', 'W', '台北縣', 'F', '嘉義縣', 'Q', '連江縣', 'Z', '宜蘭縣', 'G', '台南縣',
-# 'R', '嘉義市', 'I', '桃園縣', 'H', '高雄縣', 'S', '新竹市', 'O', '新竹縣', 'J', '屏東縣', 'T'
-
-# dictionary {key = 縣市, value = 小寫英文字母}
+# list轉成dictionary, 方便用來找縣市對應的英文字母
 locToLetter = dict(
     zip(location_str.split()[::2], location_str.lower().split()[1::2]))
 
-
-# 歷年資料夾
-
-# for d in os.listdir():
-#     # print(d)
-#     if d[:4] == 'real':
-#         continue
-
-
+# 把real開頭的資料夾的路徑放入dirs,等等要用for走過
 dirs = [d for d in os.listdir() if d[:4] == 'real']
 
-# list []
-# dictionary {}
-
+# 把每一個real開頭的資料夾，依照自定義的縣市把檔案打開，匯入到dfs裡面
 for location in location_all:
     dfs = []
-    # pd => pandas (讀取excel的)
     for d in dirs:
-        # print(d)
-        # print(locToLetter[location]+'_lvr_land_a.csv')
-        # os.path C:\Users\jack2\OneDrive\Desktop\part_time\CrawlRealEstate\read_estate1021\a_lvr_land_a.csv
         print(os.path.join(
             d, locToLetter[location] + '_lvr_land_a.csv'))
-        df = pd.read_csv(os.path.join(
-            d, locToLetter[location] + '_lvr_land_a.csv'), index_col=False, low_memory=False)
-        # print(df) #row data
+        if os.path.exists(os.path.join(
+                d, locToLetter[location] + '_lvr_land_a.csv')) == True:
+            df = pd.read_csv(os.path.join(
+                d, locToLetter[location] + '_lvr_land_a.csv'), index_col=False, low_memory=False)
 
-        # 第幾季
-        df['Q'] = d[-1]
+            # 第幾季
+            df['Q'] = d[-1]
 
-        # 在list內放入許多pandas資料結構
-        dfs.append(df.iloc[1:])
-        # print(df.iloc[1:])
+            # 在list內放入許多pandas資料結構
+            dfs.append(df.iloc[1:])
+
+    # 若是沒有該地區的資料內容都沒有新建案則跳過
+    if len(dfs) == 0:
+        continue
 
     # concat把list內的所有資料接起來
     df = pd.concat(dfs, sort=True)
 
-    # print(df)
-
-    # # 新增交易年份(西元)
+    # 新增交易年份(西元)
     # 如果‘coerce’，則無效解析將設置為NaN
-    # str to int
     df['year'] = pd.to_numeric(df['交易年月日'].str[:-4], errors='coerce') + 1911
 
-    # # # 平方公尺換成坪
+    # 平方公尺換成坪
     df['單價元平方公尺'] = df['單價元平方公尺'].astype(float)
     df['單價元坪'] = df['單價元平方公尺'] * 3.30579
 
-    # # # 建物型態
+    # 建物型態
     df['建物型態2'] = df['建物型態'].str.split('(').str[0]
 
-    # # # 刪除有備註之交易（多為親友交易、價格不正常之交易）
+    # 刪除有備註之交易（多為親友交易、價格不正常之交易）
     df = df[df['備註'].isnull()]
-    # df = df[df['建築完成年月'].notnull()]
 
-    # # # 將index改成年月日
+    df['建築完成年月'] = pd.to_numeric(df['建築完成年月'], errors='coerce')
+    df = df.dropna(subset=['建築完成年月'])
+
+    # 將index改成年月日
     df.index = pd.to_datetime(df['year'].astype(
         str) + df['交易年月日'].str[-4:], errors='coerce')
     df.sort_index(inplace=True)
 
-    print(df)
-
-    # # Part 2數據分析
-    # # 長寬
-    # plt.rcParams['figure.figsize'] = (10, 6)
-
-    # # 歷年各區平均價格
-    # # prices 是一個 dictionary, key是放哪一區, value 放每年每坪的價格
-    # prices = {}
-
-    # for district in set(df['鄉鎮市區']):
-    #     # 一個一個區 依序去執行
-    #     # print(district)
-    #     cond = (
-    #         (df['主要用途'] == '住家用')
-    #         & (df['鄉鎮市區'] == district)
-    #         & (df['單價元坪'] < df["單價元坪"].quantile(0.95))
-    #         & (df['單價元坪'] > df["單價元坪"].quantile(0.05))
-    #     )
-    #     groups = df[cond]['year']
-
-    #     # print(df[cond]['單價元坪'].astype(
-    #     #     float).groupby(groups).mean().loc[2012:])
-
-    #     prices[district] = df[cond]['單價元坪'].astype(
-    #         float).groupby(groups).mean().loc[2012:]
-
-    # print(prices)
-
-    # # 固定用法
-    # price_history = pd.DataFrame(prices)
-    # price_history.plot()
-    # plt.title('各區平均單價', fontproperties=myfont)
-    # plt.legend(prop=myfont)
-    # plt.show()
-
     # Part 2數據分析(自定義)
-    # 長寬
-    plt.rcParams['figure.figsize'] = (10, 6)
 
     # 歷年各區平均價格
-    # prices 是一個 dictionary, key是放哪一區, value 放每年每坪的價格
-    prices = {}
-
+    # building_list 是一個 dictionary, key是放哪一區, value 放那區裡面相同建築完成日期地址的list
     building_list = {}
 
     # 所有區的建物價格加總
@@ -156,32 +90,24 @@ for location in location_all:
 
     for district in set(df['鄉鎮市區']):
         # 一個一個區 依序去執行
-        # print(district)
+        # 判斷的情況
         cond = (
             (df['主要用途'] == '住家用')
             & (df['建物型態2'] == '住宅大樓')
             & (df['鄉鎮市區'] == district)
-            & (df['單價元坪'] < df["單價元坪"].quantile(0.95))
-            & (df['單價元坪'] > df["單價元坪"].quantile(0.05))
-            & ((df['建築完成年月'].astype(float) > 1090000)  # 新增
-               | (df['建築完成年月'].isnull()))
-            & (df['交易年月日'].astype(float) > 1090000)  # 新增
+            & (df['單價元坪'] < df["單價元坪"].quantile(0.95))  # 刪除離群值
+            & (df['單價元坪'] > df["單價元坪"].quantile(0.05))  # 刪除離群值
+            & ((df['建築完成年月'].astype(float) > 1090000)  # 建築完成年月日在2020之後
+               | (df['建築完成年月'].isnull()))  # 建築完成年月日為空值
+            & (df['交易年月日'].astype(float) > 1090000)  # 交易年月日在2020之後
+
         )
-        groups = df[cond]['year']
 
-        # print(df[cond]['單價元坪'].astype(
-        #     float).groupby(groups).mean().loc[2012:])
-
-        # 原本的code
-        prices[district] = df[cond]['單價元坪'].astype(
-            float).groupby(groups).mean().loc[2020:]
-
-        # 新增
         # 總共的成交金額與成交量
         total_money += df[cond]['單價元坪'].sum()
         total_building += len(df[cond]['單價元坪'])
 
-        # 新增
+        # 列出該地區資訊
         print(district, ", 單價元坪 = ", df[cond]['單價元坪'].astype(
             float).mean())
         print(district, ", 建物價格總和 = ", df[cond]['單價元坪'].sum())
@@ -189,34 +115,18 @@ for location in location_all:
         print('建物地址 ', df[cond]['土地位置建物門牌'])
 
         # 為了之後要分類社區用
-        building_group = df[cond]['建築完成年月']
-        # print(building_group)
-        # building_list[district] = df[cond]['土地位置建物門牌'].astype(
-        #     str).groupby(building_group)
-        building_list[district] = df[cond]['土地位置建物門牌'].groupby(building_group)
+        building_group = df[cond]['建築完成年月']  # 分群相同建築完成年月日的
+        building_list[district] = df[cond]['土地位置建物門牌'].groupby(
+            building_group)  # 放入dictionary中
 
-    # 新增
+    # 列出該縣市資訊
     print(location, ", 單價元坪 = ", total_money/total_building)
     print(location, ", 建物價格總和 = ", total_money)
     print(location, ", 建物數量 = ", total_building)
 
-    # 原本的code
-    print(prices)
-
-    # for i in building_list:
-    #     for key, item in building_list[i]:
-    #         # print(building_list[i].get_group(key), "\n\n")
-    #         print(item)
-
-    # 固定用法
-    price_history = pd.DataFrame(prices)
-    price_history.plot()
-    plt.title('各區平均單價', fontproperties=myfont)
-    plt.legend(prop=myfont)
-    # plt.show()
-
-    def string_similar(s1, s2):
-        return difflib.SequenceMatcher(None, s1, s2).quick_ratio()
+    # 若是該地區都沒有新建案則跳過
+    if total_money == 0:
+        continue
 
     # 處理地址資訊
     community_total = {}
@@ -231,7 +141,7 @@ for location in location_all:
             l = []
             for path in item:
                 path = path.replace(district, "").replace(location, "")
-                l.append(location+district+path)
+                l.append(location+district+path)    # 把"市"以及"區"資料加回去
 
             community_total[district].append(l)
 
@@ -243,7 +153,9 @@ for location in location_all:
 
     #存檔, w = write, r = read
     with open(location+'_community.txt', 'w') as f:
-        f.write("%s新建案社區數量:%s (since 2020)\n" % (location, community_num))
+        f.write("%s新建案數量:%s (since 2020)\n" % (location, community_num))
+        f.write("%s新建案單價元坪 = %s \n" % (location, total_money/total_building))
+
         for key in community_total.keys():
             f.write("%s\n" % (key))
             index = 1
@@ -255,68 +167,3 @@ for location in location_all:
 
             # enter的意思
             f.write("\n")
-
-    # print(community_total)
-    # # dropna丟棄空值的行列
-    # district_price = df.reset_index()[['鄉鎮市區', '單價元坪']].dropna()
-    # district_price = district_price[district_price['單價元坪'] < 2000000]
-
-    # fig = px.histogram(district_price, x="單價元坪", color="鄉鎮市區",
-    #                    marginal="rug", nbins=50)  # can be `box`, `violin`)
-
-    # # Overlay both histograms
-    # fig.update_layout(barmode='overlay')
-    # # Reduce opacity to see both histograms
-    # fig.update_traces(opacity=0.75)
-    # fig.show()
-
-    # # '店面', '套房', '其他', '農舍', '倉庫', '廠辦', '透天厝', '工廠']
-    # types = ['華廈', '公寓', '住宅大樓', '套房']
-
-    # building_type_prices = {}
-    # for building_type in types:
-    #     cond = (
-    #         (df['主要用途'] == '住家用')
-    #         & (df['單價元坪'] < df["單價元坪"].quantile(0.8))
-    #         & (df['單價元坪'] > df["單價元坪"].quantile(0.2))
-    #         & (df['建物型態2'] == building_type)
-    #     )
-    #     building_type_prices[building_type] = df[cond]['單價元坪'].groupby(
-    #         df[cond]['year']).mean().loc[2012:]
-    # pd.DataFrame(building_type_prices).plot()
-
-    # plt.title('不同建物的價格', fontproperties=myfont)
-    # plt.legend(prop=myfont)
-    # plt.show()
-
-    # # @title 價格與漲跌的相關性
-    # mean_value = price_history.mean()
-
-    # print("hello world", price_history.iloc[-1])
-
-    # gain = (price_history.iloc[-1] / price_history.iloc[0])
-    # mean = price_history.mean()
-
-    # compare = pd.DataFrame({'上漲幅度': gain, '平均價格': mean}).dropna()
-    # corr = (compare.corr().iloc[0, 1])
-
-    # print('相關性：', corr)
-    # if corr > 0:
-    #     print('意涵：價格越高越保值')
-    # else:
-    #     print('意涵：價格越低越保值')
-    # print()
-    # print('各區平均價格')
-    # print(mean.sort_values())
-
-    # gain = (price_history.iloc[-1] / price_history.iloc[0])
-    # # print("gain", gain)
-    # mean = price_history.mean()
-
-    # compare = pd.DataFrame({'上漲幅度': gain, '平均價格': mean}).dropna()
-    # # print(compare.corr().iloc[0, 1])
-
-    # compare.plot()
-    # # plt.title('aaa', fontproperties=myfont)
-    # # plt.legend(prop=myfont )
-    # plt.show()
--- website.py
+++ website.py
@@ -1,35 +1,71 @@
-
+import urllib
 import requests
 import re
+from bs4 import BeautifulSoup
 
 # 591
 # https://market.591.com.tw/
 
 # 樂居
 # https://www.leju.com.tw/
+url = 'https://www.leju.com.tw/page_search_result?oid=L5891107499320f'
+
 
 # 樂屋
-# https://www.rakuya.com.tw/realprice/result?city=0&zipcode=106&search=community&keyword=%E6%88%90%E5%BE%B7%E8%8F%81%E9%81%B8%E9%9B%86&sort=11
+# url = 'https://www.rakuya.com.tw/realprice/result?city=0&zipcode=106&search=community&keyword=%E6%88%90%E5%BE%B7%E8%8F%81%E9%81%B8%E9%9B%86&sort=11'
+
+# print(urllib.parse.unquote(url))
+
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Mobile Safari/537.36',
+# }
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Mobile Safari/537.36',
+    'referer': 'https://www.leju.com.tw/',
+    'authority': 'api.leju.com.tw',
+    'path': '/api/data/objectPageView?hoid=L5891107499320f'
+}
+
+
+r = requests.get(url, headers=headers)
+
+if r.status_code == 200:
+    print(r.content.decode('utf-8'))
+
+    # soup = BeautifulSoup(r.text, 'html.parser')
+    # household = soup.find_all("div", {"class": "point__info"})
+    # print(household[0].text)
+
+    # total_sales = soup.find_all("span", {"class": "top__numb setSearchTotal"})
+    # print(total_sales[0].text)
+
+else:
+    print("status code = ", r.status_code)
+
+# data = {
+#     'id': 'L5891107499320f',
+#     'lejuToken': '500508584d67763bae50a05247d91392'
+# }
+
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Mobile Safari/537.36',
+#     'referer': 'https://www.leju.com.tw/',
+#     'authority': 'api.leju.com.tw'
+# }
 
 
 # r = requests.get(
-#     'https://www.leju.com.tw/page_search_result?oid=L4021038888b2d9')
-# r.encoding = 'utf-8'
+#     'https://api.leju.com.tw/api/data/objectPageView?hoid=Lb365009a731c', headers=headers)
+# url = "https://api.leju.com.tw/api/search/objectData"
+# url = "https://api.leju.com.tw/api/search/TransactionsObject"
+# r = requests.post(url, data=data, headers=headers)
+
+# r.encoding = 'unicode-escape'
 # if r.status_code == 200:
-#     # print(r.content.decode('utf-8'))
-
 #     subject = re.findall(r'本社區共有',
-#                          r.content.decode('utf-8'))
-#     print(subject)
-
-
-# print("status code = ", r.status_code)
-
-
-r = requests.get(
-    'https://www.leju.com.tw/page_search_result?oid=L5891107499320f')
-r.encoding = 'utf-8'
-if r.status_code == 200:
-    print(r.content.decode('utf-8'))
-else:
-    print(r.status_code)
+#                          r.content.decode('unicode-escape'))
+#     print(r.content.decode('unicode-escape'))
+#     # print(subject)
+# else:
+#     print(r.status_code)
